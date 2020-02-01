eca4caa1a21f1bc6a5e50b7f6c1806f18e96f769
Amir Yunas <ayunas@gmail.com>
2020-01-31 18:44:47 -0600

    created a basic ls8 assembly program to load two values into the register and perform a bitwise not on the value in r0.  working properly with exception of formating input file.

--- ls8.py
+++ ls8.py
@@ -1,5 +1,5 @@
 
-from operations import LDI,PRN,TEST1,TEST2,TEST3,TEST4,TEST5,CMP,JEQ,JNE,JMP,HLT
+from operations import LDI,PRN,TEST1,TEST2,TEST3,TEST4,TEST5,CMP,JEQ,JNE,JMP,HLT,NOT
 import sys
 
 class LS8:
@@ -26,6 +26,7 @@ class LS8:
         for i in range(7): #skip the 1st 7 lines of the program
             program.readline()
         
+        
         instructions = []
         for line in program:
             byte = line.split(' #')[0]
@@ -37,32 +38,50 @@ class LS8:
         for byte in instructions:
             self.ram[address] = int(byte,2)
             address += 1
-        
-        print(self.ram)
+    
+    def not_bitwise(self,n):
+        binary = f"{n:08b}"
+        bin_list = [int(b) for b in iter(binary)]
+        # flipped = [1 for b in bin_list if b == 0 else 0 for b in bin_list]
+        flipped = ['1' if b == 0 else '0' for b in bin_list]
+        notted = "".join(flipped)
+        return int(notted,2)
 
     def ldi(self,r_address,val): #load immediate (into a cpu register)
         self.reg[r_address] = val
-        print('LDI complete.  register:', self.reg)
+        # print('LDI complete.  register:', self.reg)
     
-    def alu(self,op,reg_a,reg_b):
+    def alu(self,op,reg_a,reg_b=0):
         if op == 'CMP':
             if self.reg[reg_a] - self.reg[reg_b] < 0: #reg_a < reg_b
                 self.fl = 0b00000100  #3
-                print(self.reg[reg_a],' < ', self.reg[reg_b])
+                # print(self.reg[reg_a],' < ', self.reg[reg_b])
             elif self.reg[reg_a] - self.reg[reg_b] > 0: #reg_a > reg_b
                 self.fl = 0b00000010 #2
                 print(self.reg[reg_a], ' > ', self.reg[reg_b])
             elif self.reg[reg_a] - self.reg[reg_b] == 0:
                 self.fl = 0b00000001 #1
-                print(self.reg[reg_a], ' = ', self.reg[reg_b])
+                # print(self.reg[reg_a], ' = ', self.reg[reg_b])
             else:
-                print(f'error occured comparing {self.reg[reg_a]} and {self.reg[reg_b]}')
+                # print(f'error occured comparing {self.reg[reg_a]} and {self.reg[reg_b]}')
                 raise Exception
                 sys.exit()
+        elif op == 'ADD':
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == 'AND':
+            self.reg[reg_a] = self.reg[reg_a] & self.reg[reg_b]
+        elif op == 'OR':
+            self.reg[reg_a] = self.reg[reg_a] | self.reg[reg_b]
+        elif op == 'NOT':
+            # self.reg[reg_a] = ~self.reg[reg_a]
+            self.reg[reg_a] = self.not_bitwise(self.reg[reg_a])
+            print(self.reg[reg_a])
+            print(self.reg)    
+
     def push(self,pc):
         self.ram[self.sp] = pc
         self.sp -= 1
-        print('ram after pushing', self.ram)
+        # print('ram after pushing', self.ram)
     
     def pop(self):
         self.sp += 1
@@ -80,7 +99,7 @@ class LS8:
 
         while halted == False:
             instruction = self.ram[self.pc]
-            print(f'instruction', instruction, 'pc:', self.pc)
+            # print(f'instruction', instruction, 'pc:', self.pc)
 
             if instruction == LDI:
                 r_address = self.ram[self.pc + 1]
@@ -100,20 +119,24 @@ class LS8:
                 self.pc += 3
             
             elif instruction == JEQ: #jump to register if equal
-                print('in JEQ')
+                # print('in JEQ')
                 if self.fl == 0b00000001: #equal bit flagged
                     self.push(self.pc)
                     r_address = self.ram[self.pc + 1]
                     self.pc = self.reg[r_address]
                 else:
-                    print('fl not equal')
+                    # print('fl not equal')
                     self.pc += 2
+            elif instruction == NOT:
+                reg_a = self.ram[self.pc + 1]
+                self.alu('NOT',reg_a)
+                self.pc += 2
             
             elif instruction == TEST1:
-                print('in TEST1')
+                # print('in TEST1')
                 self.pc += 1 #evaulate the subroutine
             elif instruction == TEST2:
-                print('in TEST2')
+                # print('in TEST2')
                 self.pc += 1
             elif instruction == TEST3:
                 self.pc += 1
@@ -123,9 +146,9 @@ class LS8:
                 self.pc += 1
             
             elif instruction == JNE: #jump to register if not equal
-                print('in JNE')
+                # print('in JNE')
                 if self.fl != 0b00000001:
-                    print('last comparison values not equal JNE')
+                    # print('last comparison values not equal JNE')
                     self.push(self.pc)
                     r_address = self.ram[self.pc + 1]
                     self.pc = self.reg[r_address]
@@ -133,7 +156,7 @@ class LS8:
                     self.pc += 2
             
             elif instruction == JMP:
-                print('in JMP')
+                # print('in JMP')
                 self.push(self.pc)
                 r_address = self.ram[self.pc + 1]
                 self.pc = self.reg[r_address]
